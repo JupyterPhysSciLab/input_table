9718d2e448305d6efc56e3ff01da6c345ae46e0c
gutow <gutow@uwosh.edu>
2020-03-19 20:26:44 -0500

    Initial commit after separation from JupyterPiDAQ

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="InputTable",
+    version="0.5.0",
+    description="Predefined data input tables for Jupyter notebooks",
+    long_description=long_description,
+    author="Jonathan Gutow",
+    author_email="jgutow@new.rr.com",
+    license="GPL-3.0+",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        # 'python>=3.6',
+        'jupyter>=1.0.0',
+        #'pandas>=0.22.0'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers :: End Users',
+        'License :: GPL-3.0+',
+        'Programming Language :: Python 3 :: Javascript'
+    ]
+)
